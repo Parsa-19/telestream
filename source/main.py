9be3cff69d3394bda017f565375afb21b9684fe8
Parsa-19 <parsakhalife19@gmail.com>
2025-06-08 20:05:15 +0330

    code update

---
+++ source/main.py
@@ -0,0 +1,11 @@
+from telegram.ext import Application
+
+def main():
+	TOKEN = '8087760670:AAFn0pzO8LsttESIZ73W_Aa26TjZGQmq088'
+	application = Application.builder().token(TOKEN).concurrent_updates(True).read_timeout(30).write_timeout(30).build()
+	print("Telegram Bot started!", flush=True)
+	application.run_polling()
+
+if __name__ == '__main__':
+
+	main()
